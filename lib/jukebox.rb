164fc9aba06b07f3557930e90a3698d0e8832731
Irene Saez <bluemilk44@gmail.com>
2019-02-10 19:01:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,5 +36,5 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   song_choice = gets.chomp
-  if (1..9)to_a.include?(song_choice) 
+  if (1..9).to_a.include?(song_choice.to_i) 
 end
